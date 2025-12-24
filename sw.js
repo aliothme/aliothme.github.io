cfe8f74a91e946e8aba9e5e5ce48e1b057255211
alioth.me <76646396+aliothme@users.noreply.github.com>
2025-12-24 22:45:28 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker buat caching basic
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('pwa-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icon.png' // Ganti kalau lo punya ikon
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
